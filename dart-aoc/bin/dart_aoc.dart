8853f80bea100c51e33ecad19dcc9e6a2758a1b0
ramanverma2k <ramanverma4183@gmail.com>
2022-12-01 20:15:04 +0530

    add README & run dart create

---
+++ dart-aoc/bin/dart_aoc.dart
@@ -0,0 +1,5 @@
+import 'package:dart_aoc/dart_aoc.dart' as dart_aoc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_aoc.calculate()}!');
+}
---
+++ dart-aoc/test/dart_aoc_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_aoc/dart_aoc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
